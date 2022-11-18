425439cad0e541ad2badd4d282ce9b9f6d8776d0
bahromiy <tillanigmatov4@gmail.com>
2022-11-19 00:57:23 +0500

    added news

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-var test = +prompt("To'plagan balingizni kiriting");
+// var test = +prompt("To'plagan balingizni kiriting");
 
-if (test >= 80 && test <= 100) {
-  alert(`Siz super kontrakga qabul qilindingiz`);
-} else {
-  alert(`Afsus super kontrakda  ham o'qiy olmaysiz`);
-}
+// if (test >= 80 && test <= 100) {
+//   alert(`Siz super kontrakga qabul qilindingiz`);
+// } else if (test >= 100) {
+//   alert(`Siz kontraktga qabul qilindingiz`);
+//   var summa = +prompt(`Kontrakt summasi $1500. Qancha mablagʻingiz bor?`)
+//   if (summa >= 1500) {
+//     alert(`Mablagʻingiz yetarli`)
+//   } else {
+//     alert(`Afsuski, mablagʻingiz yetarli emas. Sizga yana $${3000 - summa} kerak`)
+//   }
+// } else {
+//     alert(`Directed by ROBERT B.WEIDE`)
+// }
 
-var money = +prompt("Super Kontrakt narxi 3000$. Sizda qancha pul bor?");
-if (money >= 3000) {
-  alert(`Pulingiz yetarli `);
+// var pul = +prompt("Super kontrakt summasi $3000. Sizda qancha mablagʻ bor?");
+// if (pul >= 3000) {
+//   alert(`Mablagʻingiz yetarli`);
+// } else {
+//   alert(
+//     `Afsuski, mablagʻingiz yetarli emas. Sizga yana $${3000 - pul} kerak`
+//   );
+// }
+
+///////////////////////////// 
+
+var ball = +prompt("To'plagan balingizni kiriting");
+if (ball >= 100 && contract <= 150) {
+  alert(`Siz kontraktga qabul qilindingiz`);
+} else if (ball >= 150) {
+  alert(`Siz Grant asosida oʻqishga qabul qilindingiz!`);
 } else {
-  alert(
-    `Afsuski pulingiz yetarli emas. Sizga yanam ${3000 - money} kerak bo'ladi`
-  );
+    alert(`Siz faqat super kontrakda oʻqiy olasiz`)
 }
